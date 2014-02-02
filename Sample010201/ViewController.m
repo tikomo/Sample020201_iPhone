50365a3d09e2484bc4f2a21d252c7f6f818855d9
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-02-02 12:59:58 +0900

    Initial Commit

---
+++ Sample010201/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Sample010201
+//
+//  Created by tikomo on 2014/02/02.
+//  Copyright (c) 2014年 tikomo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Sample010201/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample010201
+//
+//  Created by tikomo on 2014/02/02.
+//  Copyright (c) 2014年 tikomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
